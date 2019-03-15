139728558400de64776ab4423cc2d40c09dfe5ea
kaerim-cisneros <kaerim.cisneros@gmail.com>
2019-03-15 00:32:11 -0600

    progress track calculator function completed

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -12,7 +12,8 @@ class Schedule extends Component {
         const data = this.props.courses
 
         return data.map((course, index) => {
-            if(course.enrolled) {
+            if(course.enrolled && this.amountEnrolled != 5) {
+                this.amountEnrolled++;
                 return <ScheduleCourse {...course} key={index}/>
             }
         })
@@ -20,11 +21,12 @@ class Schedule extends Component {
 
 
     render() {
+        this.amountEnrolled = 0
         return (
             <div className="schedule">
                 <h1 className="schedule-title">My Schedule</h1>
                 { this.renderCourses() }
-                <ProgressTracker/>
+                <ProgressTracker amountPossible={5} amountEnrolled={this.amountEnrolled}/>
                 <Gradient/>
             </div>
         )
@@ -36,5 +38,4 @@ function mapStateToProps(state) {
         courses: state.courses
     }
 }
-
 export default connect(mapStateToProps)(Schedule);
