7257e0b727ab026b203f8f37fd64d8d22e8c95d3
kaerim-cisneros <kaerim.cisneros@gmail.com>
2019-03-18 22:30:59 -0600

    almost finished the course part and starting the style of the title on the schedule part

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -17,17 +17,20 @@ class LibraryCourse extends Component {
         this.state = {
           status: true,
           height: 0
-        }
+        };
       }
     
      handleCallback = function(status) {
-        let height = this.state.height == 0 ? 80:0;
+        let height = this.state.height == 0 ? 'auto':0;
         if(!status) {
             document.getElementById(this.id).classList.add('library-course-selected');
           } else {
             document.getElementById(this.id).classList.remove('library-course-selected');
           }
-        this.setState({ status })
+        this.setState({ 
+            status,
+            height
+         });
       }.bind(this)
     
 
@@ -36,10 +39,9 @@ class LibraryCourse extends Component {
         return (
             <div id={this.id} className="library-course">
                 <div className= "library-course-title-wrp">
-                    <label className="library-course-title">{ this.props.title }</label>
+                    <div className="library-course-title">{ this.props.title }</div>
                     { Icon('fas fa-check', 'library-course-icon') }
                 </div>
-                <div className="library-course-line" />
                 <Arrow 
                     callback={status => this.handleCallback(status)}
                     id={this.props.id} 
